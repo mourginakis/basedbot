8223eb84c17a988bdf00802cef78b6c2d9266b3a
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2024-07-22 00:21:06 -0700

    add openai keys to .env.example

--- main.py
+++ main.py
@@ -21,6 +21,9 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 
+all_time_cost: float = 0.0
+
+
 # Events
 
 @bot.event
@@ -28,23 +31,38 @@ async def on_ready():
     print(f'{bot.user} successfully logged in!')
 
 
+
 @bot.command(name="greet")
 async def greet(ctx):
     await ctx.send("Hello! I'm BasedBot!")
 
+
+
 @bot.command(name="draw")
-async def draw(ctx, *, prompt: str = None):  # The '*' tells the command to take the rest of the message as 'prompt'
+async def draw(ctx, *, prompt: str = None):
+
     if not prompt:
         await ctx.send("Error: no prompt")
         return None
     
-        # Process the prompt or perform the drawing action here
-    await ctx.send(f"Drawing: {prompt}")
+    # Process the entire prompt
+    # all_time_cost += 0.40
+    await ctx.send(f"Prompt: {prompt}")
+    await ctx.send(f"Working..... Queue now full.")
+
+    await ctx.send(f"Image Generation Cost: $0.04")
+    return None
 
 
 @bot.command(name="info")
 async def draw(ctx):
-    await ctx.send("I'm BasedBot! I can draw images for you!")
+    await ctx.send(
+        f"I'm BasedBot! I run DALLE-3.\n"
+        f"We can only run one image at a time.\n"
+        f"Max prompt length: 1000 characters.\n"
+        f"All time cost (not yet implemented): {all_time_cost:,.2f}\n"
+        f"https://cookbook.openai.com/articles/what_is_new_with_dalle_3\n"
+    )
     return None
 
 
