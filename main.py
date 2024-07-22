214a1b7e00f18fb62863dedccd6288dffc400c7c
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2024-07-22 01:22:58 -0700

    get dalle3 generation working

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from openai import OpenAI
 
 
 # Load in secrets/config
 load_dotenv()
-DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+DISCORD_BOT_TOKEN        = os.getenv("DISCORD_BOT_TOKEN")
+OPENAI_PROJECT_NAME      = os.getenv("OPENAI_PROJECT_NAME")
+OPENAI_PROJECT_API_KEY   = os.getenv("OPENAI_PROJECT_API_KEY")
 
 
 # Init the bot server/client
@@ -19,9 +22,12 @@ intents.presences = False
 intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+openai_client = OpenAI(api_key=OPENAI_PROJECT_API_KEY,
+                       project=OPENAI_PROJECT_NAME)
 
 
-all_time_cost: float = 0.0
+
+total_cost_since_bot_reboot = 0.0
 
 
 # Events
@@ -37,6 +43,15 @@ async def greet(ctx):
     await ctx.send("Hello! I'm BasedBot!")
 
 
+# dalle-3 limit is 7 images per minute
+# dalle-2 limit is 50 images per minute
+# urls will expire after 1 hour
+# https://platform.openai.com/docs/guides/images/usage
+# https://platform.openai.com/settings/organization/limits
+
+# 1024x1024, 1792x1024, or 1024x1792 for DALL·E-3 
+
+
 
 @bot.command(name="draw")
 async def draw(ctx, *, prompt: str = None):
@@ -49,8 +64,36 @@ async def draw(ctx, *, prompt: str = None):
     # all_time_cost += 0.40
     await ctx.send(f"Prompt: {prompt}")
     await ctx.send(f"Working..... Queue now full.")
+    await ctx.send(f"todo: implement a typing indicator")
+    await ctx.send(f"todo: implement a queue system")
 
-    await ctx.send(f"Image Generation Cost: $0.04")
+    revised_prompt = ""
+    image_url = ""
+
+    try:
+        response = openai_client.images.generate(
+            model="dall-e-3",
+            prompt=prompt,
+            size="1024x1024",
+            quality="standard", # or "hd"
+            n=1,
+        )
+        # todo: try catch
+        revised_prompt = response.data[0].revised_prompt
+        image_url = response.data[0].url
+    except Exception as e:
+        await ctx.send(f"Error: {e}")
+
+    global total_cost_since_bot_reboot
+    total_cost_since_bot_reboot += 0.04
+
+    await ctx.send(f"{image_url}")
+    await ctx.send(
+        f"```\n"
+        f"{revised_prompt}\n"
+        f"-- Image Generation Cost: $0.04 |  Total Cost since bot reboot: ${total_cost_since_bot_reboot:,.2f} --\n"
+        f"-- Image URLs will expire after 1 hour. (todo: fix this) --\n.\n"
+        f"```\n")
     return None
 
 
@@ -60,7 +103,6 @@ async def draw(ctx):
         f"I'm BasedBot! I run DALLE-3.\n"
         f"We can only run one image at a time.\n"
         f"Max prompt length: 1000 characters.\n"
-        f"All time cost (not yet implemented): {all_time_cost:,.2f}\n"
         f"https://cookbook.openai.com/articles/what_is_new_with_dalle_3\n"
     )
     return None
