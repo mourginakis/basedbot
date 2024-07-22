daf625f69b659f06a35af404ad515f04cd247683
Ioannis Mourginakis <ioannis.mourginakis@gmail.com>
2024-07-21 23:45:20 -0700

    add commands greet, draw, info

--- main.py
+++ main.py
@@ -28,17 +28,43 @@ async def on_ready():
     print(f'{bot.user} successfully logged in!')
 
 
+@bot.command(name="greet")
+async def greet(ctx):
+    await ctx.send("Hello! I'm BasedBot!")
+
+@bot.command(name="draw")
+async def draw(ctx, *, prompt: str = None):  # The '*' tells the command to take the rest of the message as 'prompt'
+    if not prompt:
+        await ctx.send("Error: no prompt")
+        return None
+    
+        # Process the prompt or perform the drawing action here
+    await ctx.send(f"Drawing: {prompt}")
+
+
+@bot.command(name="info")
+async def draw(ctx):
+    await ctx.send("I'm BasedBot! I can draw images for you!")
+    return None
+
+
+
+
+
+
+
+# ---------------- We probably won't use this ----------------
+
 @bot.event
 async def on_message(message):
     # Make sure the Bot doesn't respond to its own messages
     if message.author == bot.user: 
-        return
+        return None
     
     # Check if the message is in the "basedbot" channel
     if message.channel.name == 'basedbot':
-        await message.channel.send(f'Received message: {message.content}')
-        # await message.channel.send(f'From: {str(message)}')
-        # await message.channel.send(f'Author: {str(message.content)}')
+        pass
+        # await message.channel.send(f"echo: {message.content}")
 
     await bot.process_commands(message)
 
